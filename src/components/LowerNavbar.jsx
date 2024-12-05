666298de22747b27b79f93d9fda3c1de1dbc9254
SOUVIK HAZRA <hazrasouvik230@gmail.com>
2024-12-05 09:10:27 +0530

    Made changes

--- src/components/LowerNavbar.jsx
+++ src/components/LowerNavbar.jsx
@@ -1,3 +1,79 @@
+// import React, { useEffect, useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+
+// const LowerNavbar = () => {
+//   const navigate = useNavigate();
+
+//   const [userName, setUserName] = useState();
+//   const [activeNav, setActiveNav] = useState("/home");
+
+//   useEffect(() => {
+//     const fullName = localStorage.getItem("userName");
+//     if (fullName) {
+//       setUserName(fullName.split(" ")[0]);
+//     }
+//   }, []);
+
+//   const handleNavClick = (path) => {
+//     setActiveNav(path);
+//     navigate(path);
+//   };
+
+//   return (
+//     <div className='lowerNavbar'>
+//       <div className='logo'>
+//         <img src="./LOGO 1.png" alt="logo" onClick={() => handleNavClick("/home")} />
+//       </div>
+
+//       <div className='content'>
+//         <p
+//           className={`navItems ${activeNav === "/home" ? "active" : ""}`}
+//           onClick={() => handleNavClick("/home")}
+//         >
+//           Home
+//         </p>
+//         <p className="navItems">
+//           Browse Menu
+//         </p>
+//         <p className="navItems">
+//           Special Offers
+//         </p>
+//         <p
+//           className={`navItems ${activeNav === "/product" ? "active" : ""}`}
+//           onClick={() => handleNavClick("/product")}
+//         >
+//           Restaurants
+//         </p>
+//         <p className="navItems">
+//           Track Order
+//         </p>
+//         <div
+//           className='loginCheck'
+//           onClick={() => {
+//             if (!userName) {
+//               navigate("/");
+//             } else {
+//               navigate("/profile");
+//             }
+//           }}
+//         >
+//           <img src="./Male User.png" alt="user" />
+//           <span>{userName ? `Hey ${userName}` : "Login/Signup"}</span>
+//         </div>
+//       </div>
+
+//       <div className='moreBtn'>
+//         <img src="./Menu.png" alt="" onClick={() => console.log('object')} />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default LowerNavbar;
+
+
+
+
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
@@ -6,6 +82,7 @@ const LowerNavbar = () => {
 
   const [userName, setUserName] = useState();
   const [activeNav, setActiveNav] = useState("/home");
+  const [isMoreVisible, setIsMoreVisible] = useState(false);
 
   useEffect(() => {
     const fullName = localStorage.getItem("userName");
@@ -63,8 +140,21 @@ const LowerNavbar = () => {
       </div>
 
       <div className='moreBtn'>
-        <img src="./Menu.png" alt="" onClick={() => console.log('object')} />
+        <img src="./Menu.png" alt="" onClick={() => setIsMoreVisible((prev) => !prev)} />
       </div>
+      {isMoreVisible && (
+        <div className="moreBox">
+          <p onClick={() => {navigate("/home");
+            setIsMoreVisible((prev) => !prev);
+          }}>Home</p>
+          <p>Browse Menu</p>
+          <p>Special Offers</p>
+          <p onClick={() => {navigate("/product");
+            setIsMoreVisible((prev) => !prev);
+          }}>Restaurants</p>
+          <p>Track Order</p>
+        </div>
+      )}
     </div>
   );
 };
